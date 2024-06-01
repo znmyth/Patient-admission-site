3f79b16fec70d3e7d53402ec3c7272edf9c47595
znmyth <znmyth@gmail.com>
2024-06-01 09:59:59 +0330

    add swiper consultation and comments and Journal

--- script-Landing-Page.js
+++ script-Landing-Page.js
@@ -89,3 +89,67 @@ const swiper1 = new Swiper('.swiper1', {
   },
 
 });
+
+
+const swiper2 = new Swiper('.swiper2', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  rtl: true,
+
+
+  slidesPerView: 1,
+  spaceBetween: -700,
+
+  breakpoints: {
+      767: {
+          slidesPerView: 2,
+      },
+      991: {
+          slidesPerView: 3,
+      },
+  },
+
+});
+
+const swiper3 = new Swiper('.swiper3', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  rtl: true,
+
+
+  slidesPerView: 1,
+  spaceBetween: -360,
+
+  breakpoints: {
+      767: {
+          slidesPerView: 2,
+      },
+      991: {
+          slidesPerView: 3,
+      },
+  },
+
+});
+
+const swiper4 = new Swiper('.swiper4', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  rtl: true,
+
+
+  slidesPerView: 1,
+  spaceBetween: -600,
+
+  breakpoints: {
+      767: {
+          slidesPerView: 2,
+      },
+      991: {
+          slidesPerView: 3,
+      },
+  },
+
+});
